aa2eed8cc25f6ed81dac1c3f10926cf991d8a43c
Sagar Patil <SP873747@gmail.com>
2024-03-27 18:43:49 +0530

    added one more line

--- Main.java
+++ Main.java
@@ -3,8 +3,10 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is java program");
+        System.out.println("Added one more line");
         System.out.println("Output from java program");
         System.out.println(new Date());
         System.out.println("Bye from java.");
+
     }
 }
